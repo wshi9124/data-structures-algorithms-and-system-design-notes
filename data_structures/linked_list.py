aaa7cad0baf924743adb27b7c8460f9987291b61
Willie Shi <wshi9124@gmail.com>
2022-11-21 16:15:42 -0500

    notes

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -16,8 +16,47 @@ Self Referencial Object- definition of node is includes the node itself
 """
 
 class Node:
+    """
+    An object for storing a single node to a linked list.
+    Models two attributes- data and the link to the next node in the list 
+    """
+
     data = None
     next_node = None 
 
     def __init__(self, data):
         self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" %self.data 
+
+N1= Node(10)
+print(N1)
+N2= Node(20)
+N1.next_node = N2
+print(N1.next_node)
+
+class LinkedList:
+    """
+    Makes sure new list is alwasy empty
+    Singly Linked List
+    """
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        """
+        Returns the number of nodes in the list 
+        """
+        current = self.head
+        count= 0
+        """
+        same as while count != None:
+        """
+        while current:
+            count += 1
+            current = current.next_node
+
