bfe6b416eb1c47e1e7608873c08fa84d78451643
Willie Shi <wshi9124@gmail.com>
2023-01-08 17:53:59 -0500

    Java notes

--- java_notes/Main.java
+++ java_notes/Main.java
@@ -1,7 +1,8 @@
 package java_notes;
-
+import java.util.Arrays;
 import java.util.Date;
 import java.awt.*;
+import java.text.NumberFormat;
 
 // We need a return type in Java 
 // Void is a reserve keyword in Java 
@@ -48,8 +49,49 @@ public class Main {
       point2.x = 2;
       System.out.println(point1);
       // initialize string .length() .indexOf()(return -1 if index doesnt exist) .replace(target, replacement) .toLowerCase() .trim()
+      // if you wanna use quotation marks we use back slash \
       String message = "hello"; 
       System.out.println(message);
+      // Arrays have a fixed length in Java 5 is the length of the array 
+      // .length() and .sort() important 
+      int [] numbers = new int[5];
+      numbers[0] = 2;
+      System.out.println(Arrays.toString(numbers));
+      // Array part 2 use curly brackets if you know what is inside the array
+      int [] numbers2 = {1,2,3,4,5};
+      System.out.println(Arrays.toString(numbers2));
+      // 2 dimensional arrays row x column
+      int [][] twoDimensionArray = new int[2][3];
+      int [][] twoDimensionArray2 = {{1,2,3}, {1,2,3}};
+      System.out.println(Arrays.deepToString(twoDimensionArray));
+      System.out.println(Arrays.deepToString(twoDimensionArray2));
+      //constants final- makes value not be able to change (By convention final constants are capitalized)
+      final float PI = 3.14F;
+      System.out.println(PI);
+      // diving by doubles and floats 
+      double result = (double)10/(double)3;
+      float result2 = (float)10/(float)3;
+      System.out.println(result);
+      System.out.println(result2);
+      //Integer.parseInt(), Double.parseDouble() 
+      String x = "1";
+      int y = Integer.parseInt(x) + 2;
+      System.out.println(x);
+      System.out.println(y);
+      // Math.round(), Math.ceil(), Math.floor(), Math.min(x,y), Math.max(x,y), Math.random() * range you want it to produce
+      int round = Math.round(1.7F);
+      int ceiling = (int)Math.ceil(1.1F);
+      double random = Math.random() * 100;
+      System.out.println(round);
+      System.out.println(ceiling);
+      System.out.println(random);
+      // formatting Numbers- NumberFormat.getCurrencyInstance() and NumberFormat.getPercentInstance()
+      //Two ways to write it
+      NumberFormat currency = NumberFormat.getCurrencyInstance();
+      String currencyResult = currency.format(123456.847);
+      String percentResult = NumberFormat.getPercentInstance().format(.9);
+      System.out.println(currencyResult);
+      System.out.println(percentResult);
    } 
 } 
  
