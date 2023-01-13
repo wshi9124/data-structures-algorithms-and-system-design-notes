1dd49054cbe0c1232ee66b007ac8a11398ded13a
Willie Shi <wshi9124@gmail.com>
2023-01-13 16:48:17 -0500

    mortgagecalculator

--- java_notes/MortgageCalculatorPractice.java
+++ java_notes/MortgageCalculatorPractice.java
@@ -1,9 +1,32 @@
 package java_notes;
+import java.util.Scanner;
+import java.text.NumberFormat;
 
 class MortgageCalculatorPractice {
     public static void main(String[] args) {
-        int age = 20;
-        System.out.println(age);
+        // avoid magic numbers 
+        final byte MONTHS_IN_YEARS = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");    
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEARS;
+        
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEARS;
+
+        double mortgage = principal 
+                        * (monthlyInterest * Math.pow(1+ monthlyInterest, numberOfPayments)
+                        / (Math.pow(1+ monthlyInterest, numberOfPayments)) -1);
+        
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+
     }
 
 }
