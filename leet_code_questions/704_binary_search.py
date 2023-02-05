2a46afe9e7b2d3ae3f7de2d61298e5e36d541302
Willie Shi <wshi9124@gmail.com>
2023-02-04 19:19:55 -0500

    704

--- leet_code_questions/704_binary_search.py
+++ leet_code_questions/704_binary_search.py
@@ -33,4 +33,16 @@ def search_binary(nums, target):
     :type target: int
     :rtype: int
     """
+    first = 0 
+    last = len(nums) -1
+
+    while first <= last:
+        midpoint = (first + last) //2
+        if target == nums[midpoint]:
+            return midpoint
+        elif target >= nums[midpoint]:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+    return -1
     
