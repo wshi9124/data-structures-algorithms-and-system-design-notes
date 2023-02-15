832d35e3e07cee3bfd48876eddaded5c319f5a48
Willie Shi <wshi9124@gmail.com>
2023-02-14 21:37:14 -0500

    1046

--- leet_code_questions/1046_last_stone_weight.py
+++ leet_code_questions/1046_last_stone_weight.py
@@ -22,3 +22,20 @@ Input: stones = [1]
 Output: 1
 """
 
+def lastStoneWeight(self, stones):
+    """
+    :type stones: List[int]
+    :rtype: int
+    """
+    for i in range(len(stones)):
+        stones[i] *= -1
+    
+    heapq.heapify(stones)
+
+    while len(stones) > 1:
+        first = heapq.heappop(stones)
+        second = heapq.heappop(stones)
+        if second > first:
+            heapq.heappush(stones, first - second)
+    stones.append(0)
+    return abs(stones[0])
