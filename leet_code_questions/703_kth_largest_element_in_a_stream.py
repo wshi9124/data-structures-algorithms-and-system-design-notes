67b68514d85e00bea977b3aea130f3337501fbcf
Willie Shi <wshi9124@gmail.com>
2023-02-17 20:59:11 -0500

    703

--- leet_code_questions/703_kth_largest_element_in_a_stream.py
+++ leet_code_questions/703_kth_largest_element_in_a_stream.py
@@ -29,11 +29,19 @@ def __init__(self, k, nums):
     :type k: int
     :type nums: List[int]
     """
-    
+    self.minHeap = nums
+    self.k = k
+    heapq.heapify(self.minHeap)
+    while len(self.minHeap) > k:
+        heapq.heappop(self.minHeap)
+
 
 def add(self, val):
     """
     :type val: int
     :rtype: int
     """
-        
+    heapq.heappush(self.minHeap, val)
+    if len(self.minHeap) > self.k:
+        heapq.heappop(self.minHeap)
+    return self.minHeap[0]
