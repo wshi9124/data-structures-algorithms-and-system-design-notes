d86aebd1af2d76d21592f80374e1fd7c4a715707
Willie Shi <wshi9124@gmail.com>
2023-02-22 21:17:00 -0500

    143

---
+++ leet_code_questions/143_reorder_list.py
@@ -0,0 +1,52 @@
+"""
+You are given the head of a singly linked-list. The list can be represented as:
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Example 2:
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+"""
+
+def reorderList(self, head):
+    """
+    :type head: ListNode
+    :rtype: None Do not return anything, modify head in-place instead.
+    """
+    slow = head
+    fast = head.next
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    second = slow.next
+    slow.next = None
+    prev = None
+    #reverse second half
+    while second:
+        temp = second.next
+        second.next = prev
+        prev = second
+        second = temp
+    
+    #merge first and second
+
+    first = head
+    second = prev
+
+    while second:
+        temp1, temp2 = first.next , second.next
+        first.next = second
+        second.next = temp1
+        first = temp1
+        second = temp2
+    
+
