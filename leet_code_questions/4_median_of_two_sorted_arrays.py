8ffef24d2fa0327ad6c504ab4b51b8addab3146b
Willie Shi <wshi9124@gmail.com>
2023-03-12 22:07:57 -0400

    4. Median of Two Sorted Arrays

---
+++ leet_code_questions/4_median_of_two_sorted_arrays.py
@@ -0,0 +1,44 @@
+"""
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+
+Example 2:
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+"""
+def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    a, b = nums1, nums2
+    total = len(a) + len(b) 
+    half = total //2
+
+    if len(a) > len(b):
+        a,b = b,a
+    
+    l, r = 0, len(a) -1
+    while True:
+        i = (l +r)//2 #a
+        j = total - i - 2 #b
+        #float infinity for edge case if none of the numbers are in the first array
+        aLeft = a[i] if i >= 0 else float("-infinity")
+        aRight = a[i+1] if i+1 < len(a) else float("infinity")
+        bLeft = b[j] if j >= 0 else float("-infinity")
+        bRight = b[j+1] if j+1 < len(b) else float("infinity")
+
+        if aLeft <= bRight and bLeft <= aRight:
+            if total % 2 != 0:
+                return min(aRight, bRight)
+            return max(aLeft, bLeft) + min(aRight, bRight) /2
+        elif aLeft > bRight:
+            r = i -1
+        else:
+            l = i + 1
+
+
+
+    
