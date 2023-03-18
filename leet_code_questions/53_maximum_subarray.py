ca1037907286a85edf6bd52986e7b4210edb5e9d
Willie Shi <wshi9124@gmail.com>
2023-03-18 16:52:13 -0400

    53

---
+++ leet_code_questions/53_maximum_subarray.py
@@ -0,0 +1,29 @@
+"""
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+Example 1:
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Example 2:
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+
+Example 3:
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+"""
+
+def maxSubArray(self, nums):
+    maxSub = nums[0]
+    curSum = 0
+
+    for n in nums:
+        if curSum < 0:
+            curSum = 0
+        curSum += n
+        maxSub= max(maxSub, curSum)
+    return maxSub
