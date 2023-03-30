1446ce24e257253896ce80ac5e87d50f905a72d6
Willie Shi <wshi9124@gmail.com>
2023-03-30 16:13:59 -0400

    25

--- leet_code_questions/25_reverse_nodes_in_k-Group.py
+++ leet_code_questions/25_reverse_nodes_in_k-Group.py
@@ -13,3 +13,29 @@ Output: [3,2,1,4,5]
 """
 
 def reverseKGroup(self, head, k):
+    dummy = ListNode(0, head)
+    groupPrev = dummy 
+    while True:
+        kth = self.getKth(groupPrev, k)
+        if not kth:
+            break
+        groupNext = kth.next
+        #reverse groups
+        prev, curr = kth.next, groupPrev.next
+        while curr != groupNext:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        #becasue kth is the last node in our group we now want it to be the first node in our group
+        temp = groupPrev.next
+        groupPrev.next = kth
+        groupPrev = temp
+    return dummy.next
+    
+
+def getKth(self, curr, k):
+    while curr and k > 0:
+        curr = curr.next
+        k -= 1
+    return curr
