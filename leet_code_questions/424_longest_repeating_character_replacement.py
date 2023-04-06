96961082137a6be9385f86104627c7b38be30811
Willie Shi <wshi9124@gmail.com>
2023-04-06 01:17:13 -0400

    424

--- leet_code_questions/424_longest_repeating_character_replacement.py
+++ leet_code_questions/424_longest_repeating_character_replacement.py
@@ -14,11 +14,29 @@ Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
 The substring "BBBB" has the longest repeating letters, which is 4.
 """
 def characterReplacement(self, s, k):
-    count = {}
-    result = 0
-    l = 0
+    # count = {}
+    # l = 0
+    # result = 0
 
+    # for r in range(len(s)):
+    #     count[s[r]] = 1 + count.get(s[r], 0)
+    #     #while window is not valid 
+    #     while (r - l + 1) - max(count.values()) > k:
+    #         count[s[l]] -= 1
+    #         l += 1
+    #     result = max(result, r - l +1)
+    # return result
+
+    count = {}
+    l = 0
+    result = 0
+    maxF = 0
     for r in range(len(s)):
         count[s[r]] = 1 + count.get(s[r], 0)
-
-        result = max(result, r - l +1)
+        maxF = max(maxF, count[s[r]])
+        # while window is invalid
+        while (r - l + 1) - maxF > k:
+            count[s[l]] -= 1
+            l += 1
+        result = max(result, r - l + 1)
+    return result
