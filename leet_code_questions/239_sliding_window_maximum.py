7aec532fdc3db0517ad3973dbb7cde23744b54b1
Willie Shi <wshi9124@gmail.com>
2023-04-09 04:29:27 -0400

    239

---
+++ leet_code_questions/239_sliding_window_maximum.py
@@ -0,0 +1,47 @@
+"""
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+Return the max sliding window.
+
+Example 1:
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+"""
+
+def maxSlidingWindow(self, nums, k):
+    # Monotonically Decreasing Que
+    # o(n) time
+    output = []
+    q = collections.deque() #holds only index
+
+    l = r = 0
+
+    while r < len(nums):
+        # pop smaller values from q
+        while q and nums[q[-1]] < nums[r]:
+            q.pop()
+        q.append(r)
+
+        # remove left val from window
+        if l > q[0]:
+            q.popleft()
+
+        if (r + 1) >= k:
+            output.append(nums[q[0]])
+            l += 1
+        r += 1
+
+    return output
+            
