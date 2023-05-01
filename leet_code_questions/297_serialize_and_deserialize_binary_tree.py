a2141b86564f724fdb8e7e00bee435d1b338b52c
Willie Shi <wshi9124@gmail.com>
2023-04-30 20:13:08 -0400

    297 and 208

--- leet_code_questions/297_serialize_and_deserialize_binary_tree.py
+++ leet_code_questions/297_serialize_and_deserialize_binary_tree.py
@@ -11,4 +11,41 @@ Example 2:
 Input: root = []
 Output: []
 """
+def serialize(self, root):
+    """Encodes a tree to a single string.
+    
+    :type root: TreeNode
+    :rtype: str
+    """
+    result = []
+    def dfs(node):
+        if not node:
+            result.append("N")
+            return
+        result.append(str(node.val))
+        dfs(node.left)
+        dfs(node.right)
+    dfs(root)
+    return ",".join(result)
+
+def deserialize(self, data):
+    """Decodes your encoded data to tree.
+    
+    :type data: str
+    :rtype: TreeNode
+    """
+    vals = data.split(",")
+    self.i = 0
+    def dfs():
+        if vals[self.i] == "N":
+            self.i += 1
+            return None
+        node = TreeNode(int(vals[self.i]))
+        self.i += 1
+        node.left = dfs()
+        node.right = dfs()
+        return node
+    return dfs()
+    
+
 
