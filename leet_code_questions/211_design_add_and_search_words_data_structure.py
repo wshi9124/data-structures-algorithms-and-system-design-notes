d51d1ee7dada249ec9d3d715f07b8fb9a61c6132
Willie Shi <wshi9124@gmail.com>
2023-05-03 22:53:40 -0400

    211

--- leet_code_questions/211_design_add_and_search_words_data_structure.py
+++ leet_code_questions/211_design_add_and_search_words_data_structure.py
@@ -23,3 +23,45 @@ wordDictionary.search(".ad"); // return True
 wordDictionary.search("b.."); // return True
 """
 
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def addWord(self, word):
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.end = True
+
+    def search(self, word):
+        # recursive j is index 
+        def dfs(j, root):
+            curr = root
+
+            for i in range(j, len(word)):
+                c = word[i]
+
+                if c == ".":
+                    for child in curr.children.values():
+                        # we are passing i + 1 becasue we are going down a child
+                        if dfs(i + 1, child):
+                             #if we find a path that matches we immediately return True
+                            return True
+                    return False
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+            return curr.end
+        return dfs(0, self.root)
+            
+                
+
