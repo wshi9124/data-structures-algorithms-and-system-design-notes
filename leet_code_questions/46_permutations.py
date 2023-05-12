6dde399c86b67cd682c2fcd23859b2381d1d273b
Willie Shi <wshi9124@gmail.com>
2023-05-11 22:47:56 -0400

    90

--- leet_code_questions/46_permutations.py
+++ leet_code_questions/46_permutations.py
@@ -15,6 +15,7 @@ Output: [[1]]
 """
 
 def permute(self, nums):
+    #result = !len(nums) ex: length 3 = 6 results (3 * 2 * 1)
     result = []
 
     if len(nums) == 1:
@@ -26,4 +27,15 @@ def permute(self, nums):
     for i in range(len(nums)):
         n = nums.pop(0)
 
-        
+        perms = self.permute(nums)
+
+        #[2,3,1] [3,2,1] we are appending the number we pop into the perm
+        for perm in perms:
+            perm.append(n)
+        result.extend(perms)
+        nums.append(n)
+    
+    return result 
+
+
+
