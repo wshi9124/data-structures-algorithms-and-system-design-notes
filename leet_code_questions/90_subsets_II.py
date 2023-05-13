885fb432fd81a22e3412638de904826ca43e05b5
Willie Shi <wshi9124@gmail.com>
2023-05-12 22:12:29 -0400

    90

--- leet_code_questions/90_subsets_II.py
+++ leet_code_questions/90_subsets_II.py
@@ -13,3 +13,27 @@ Input: nums = [0]
 Output: [[],[0]]
  
 """
+
+def subsetsWithDup(self, nums):
+     # time complexity n * 2**n
+
+    result = []
+    nums.sort()
+
+    def backTrack(i, subset):
+        if i == len(nums):
+            result.append(subset.copy())
+            # result.append(subset[::]) works as well
+            return
+
+        # all subsets that include nums[i]
+        subset.append(nums[i])
+        backTrack(i + 1, subset)
+        subset.pop()
+
+        # all subsets that dont include nums[i]
+        while i + 1 < len(nums) and nums[i + 1] == nums[i]:
+            i += 1
+        backTrack(i+1, subset)
+    backTrack(0, [])
+    return result 
