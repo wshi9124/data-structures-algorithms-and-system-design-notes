2e338fe04bf834c058c6e9f9070d5fdc9567089b
Willie Shi <wshi9124@gmail.com>
2023-06-01 20:32:29 -0400

    261

--- leet_code_questions/261_graph_valid_tree.py
+++ leet_code_questions/261_graph_valid_tree.py
@@ -11,3 +11,34 @@ Input: n = 5, edges = [[0,1],[1,2],[2,3],[1,3],[1,4]]
 Output: false
 """
 
+def validTree(self, n, edges):
+    # Trees are not allowed to have loops
+    # Trees need to have every node connected 
+
+    if not n:
+        return True
+    
+    adj = {i: [] for i in range(n)}
+
+    for n1, n2 in edges:
+        adj[n1].append(n2)
+        adj[n2].append(n1)
+    
+    visit = set()
+
+    # i is node we are visiting, prev is previous node so that we don't get false positives
+    def dfs(i, prev):
+        if i in visit:
+            return False
+
+        visit.add(i)
+
+        for j in adj[i]:
+            if j == prev:
+                continue
+            if not dfs(j, i):
+                return False
+
+        return True
+
+    return dfs(0, None) and len(visit) == n
