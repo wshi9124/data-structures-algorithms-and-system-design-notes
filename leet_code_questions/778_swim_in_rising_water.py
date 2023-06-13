7ee7b265e2c792563f19739393e94eb2a850ddb6
Willie Shi <wshi9124@gmail.com>
2023-06-12 22:49:55 -0400

    778

--- leet_code_questions/778_swim_in_rising_water.py
+++ leet_code_questions/778_swim_in_rising_water.py
@@ -20,3 +20,30 @@ We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
 """
 
 def swimInWater(self, grid):
+    # greedy modify version of djikstras algorithm
+    # n**2 logn time complexity 
+    # minHeap bfs
+
+    n = len(grid)
+    visit = set()
+    minHeap = [[grid[0][0], 0, 0]] #[time/max-height, r, c]
+    directions = [(0,1), (0,-1), (1,0), (-1,0)]
+    visit.add(0,0)
+
+    while minHeap:
+        t, r, c = heapq.heappop(minHeap)
+
+        if r == n -1 and c == n - 1:
+            return t
+        
+        for dr, dc in directions:
+            neiR, neiC = r + dr, c + dc
+            if (neiR < 0 or neiC < 0 or
+            neiR == n or neiC == n or
+            (neiR, neiC) in visit):
+                continue
+            visit.add((neiR, neiC))
+            heapq.heappush(minHeap, [max(t, grid[neiR][neiC]), neiR, neiC])
+        
+
+
