adea7664b6f7d61f8c0cdba49a6fc7d19a935703
Willie Shi <wshi9124@gmail.com>
2023-06-14 23:47:35 -0400

    269

--- leet_code_questions/269_alien_dictionary.py
+++ leet_code_questions/269_alien_dictionary.py
@@ -19,5 +19,42 @@ Explanation: The order is invalid, so return "".
 """
 
 def alienOrder(self, words):
-    adj = {c:set() for w in words for c in w}
+    # topological sort  
+    # if there is a cycle we return ""
+
+    adj = {c: set() for w in words for c in w}
+
+    for i in range(len(words) - 1):
+        w1, w2 = words[i], words[i + 1]
+        minLen = min(len(w1), len(w2))
+        # if prefix of w1 and w2 are the same but w2 is longer 
+        if w1[:minLen] == w2[:minLen] and len(w1) > len(w2):
+            return ""
+        for j in range(minLen):
+            #we want the first different character
+            if w1[j] != w2[j]:
+                adj[w1[j]].add(w2[j])
+                break
     
+    visit = {} #False = visited, True = visited and current path
+    result = [] #list initially then we join and reverse at the end 
+
+    def dfs(c):
+        # loop detection if c in vist it will return True
+        if c in visit:
+            return visit[c]
+        visit[c] = True
+        for nei in adj[c]:
+            if dfs(nei):
+                return True
+        visit[c] = False
+        result.append(c)
+    
+    for c in adj:
+        if dfs(c):
+            return ""
+
+    result.reverse()
+    return "".join(result)        
+
+
