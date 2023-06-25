b7bdbc0c48e9a3f80c4e3bf51e446ec66b879a17
Willie Shi <wshi9124@gmail.com>
2023-06-24 21:20:02 -0400

    5

---
+++ leet_code_questions/5_longest_palindromic_substring.py
@@ -0,0 +1,33 @@
+"""
+Given a string s, return the longest 
+palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+"""
+
+def longestPalindrome(self, s):
+    result = ""
+    resultLen = 0
+
+    for i in range(len(s)):
+        l, r = i, i 
+        while l > 0 and r < len(s) and s[l] == s[r]:
+            result = s[l:r+1]
+            resultLen = (r - l + 1)
+        l -= 1
+        r += 1
+
+        l, r - i, i + 1
+        while l > 0 and r < len(s) and s[l] == s[r]:
+            result = s[l:r+1]
+            resultLen = (r - l + 1)
+        l -= 1
+        r += 1
+    return result
