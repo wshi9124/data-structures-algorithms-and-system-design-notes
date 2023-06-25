d5147a270f41ab9cd576d575240d3f3a4f3692cc
Willie Shi <wshi9124@gmail.com>
2023-06-24 21:26:38 -0400

    5

--- leet_code_questions/5_longest_palindromic_substring.py
+++ leet_code_questions/5_longest_palindromic_substring.py
@@ -17,17 +17,21 @@ def longestPalindrome(self, s):
     resultLen = 0
 
     for i in range(len(s)):
+        #odd length
         l, r = i, i 
-        while l > 0 and r < len(s) and s[l] == s[r]:
-            result = s[l:r+1]
-            resultLen = (r - l + 1)
-        l -= 1
-        r += 1
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > resultLen:
+                result = s[l:r+1]
+                resultLen = (r - l + 1)
+            l -= 1
+            r += 1
 
-        l, r - i, i + 1
-        while l > 0 and r < len(s) and s[l] == s[r]:
-            result = s[l:r+1]
-            resultLen = (r - l + 1)
-        l -= 1
-        r += 1
+        #even length
+        l, r = i, i + 1
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > resultLen:
+                result = s[l:r+1]
+                resultLen = (r - l + 1)
+            l -= 1
+            r += 1
     return result
