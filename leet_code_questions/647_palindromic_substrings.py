8bb504495e9179102dca4c5a1c546bd59b5b3714
Willie Shi <wshi9124@gmail.com>
2023-06-25 11:07:55 -0400

    647

--- leet_code_questions/647_palindromic_substrings.py
+++ leet_code_questions/647_palindromic_substrings.py
@@ -14,3 +14,18 @@ Output: 6
 Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 """
 
+def countSubstrings(self, s):
+    result = 0
+    for i in range(len(s)):
+        result += self.countPali(s, i, i)
+        result += self.countPali(s, i, i + 1)
+    return result 
+
+
+def countPali(self, s, l, r):
+    result = 0
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        result += 1
+        l -= 1
+        r += 1
+    return result 
