c07873792444934eb83d50517603395ae0396a23
Willie Shi <wshi9124@gmail.com>
2023-06-27 22:45:02 -0400

    91

--- leet_code_questions/91_decode_ways.py
+++ leet_code_questions/91_decode_ways.py
@@ -28,4 +28,18 @@ Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is di
 """
 
 def numDecodings(self, s):
+    # dynamic programming
+    dp = {len(s): 1}
     
+    for i in range(len(s) - 1, -1, -1):
+        print(i)
+        if s[i] == "0":
+            dp[i] = 0
+        else:
+            dp[i] = dp[i + 1]
+
+        if i + 1 < len(s) and (
+            s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
+        ):
+            dp[i] += dp[i + 2]
+    return dp[0]
