33b7a0fd72f91ae58637f850290c3c847e841e5d
Willie Shi <wshi9124@gmail.com>
2023-07-04 12:56:50 -0400

    139

--- leet_code_questions/139_word_break.py
+++ leet_code_questions/139_word_break.py
@@ -19,4 +19,15 @@ Output: false
 """
 
 def wordBreak(self, s, wordDict):
+    dp = [False] * (len(s) + 1)
+    dp[len(s)] = True
+
+    for i in range(len(s)- 1, -1, -1):
+        for w in word:
+            # (i + len(w) used to make algorithm more efficient)
+            if ((i + len(w))<= len(s) and s[i: i + len(w)] == w):
+                dp[i] = dp[i + len(w)]
+            if dp[i] == True:
+                break
+    return dp[0]
     
