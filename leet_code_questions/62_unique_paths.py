50bdb8f47842fe48cb72d7ff508f553aa44446bd
Willie Shi <wshi9124@gmail.com>
2023-07-06 15:05:09 -0400

    62

--- leet_code_questions/62_unique_paths.py
+++ leet_code_questions/62_unique_paths.py
@@ -23,10 +23,10 @@ def uniquePaths(self, m, n):
     #bottom row
     row = [1] * n
 
-    for i in range(m - 1, -1, -1):
+    for i in range(m - 1):
         newRow = [1] * n
         for j in range(n - 2, -1, -1):
-            newRow[i] = row[j] + newRow[j+1]
+            newRow[j] = row[j] + newRow[j+1]
         row = newRow
     
     return row[0]
