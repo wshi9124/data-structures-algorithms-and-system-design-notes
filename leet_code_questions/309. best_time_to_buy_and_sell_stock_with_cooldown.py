186ccfe08c0c5545bfadbee9d4d27e2ddc92fdbe
Willie Shi <wshi9124@gmail.com>
2023-07-19 14:33:46 -0400

    309

--- leet_code_questions/309. best_time_to_buy_and_sell_stock_with_cooldown.py
+++ leet_code_questions/309. best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -21,3 +21,22 @@ def maxProfit(self, prices):
 
     #caching using hashmap
     dp = {} 
+
+    def dfs(i, buying):
+        if i >= len(prices):
+            return 0
+        
+        if (i, buying) in dp:
+            return dp[(i, buying)]
+        
+        cooldown = dfs(i + 1, buying)
+        if buying:
+            buy = dfs(i + 1, not buying) - prices[i]
+            dp[(i, buying)] = max(buy, cooldown)
+        else:
+            sell = dfs(i + 2, not buying) + prices[i]
+            dp[(i, buying)] = max(sell, cooldown)
+        
+        return dp[(i, buying)]
+    
+    return dfs(0, True)
