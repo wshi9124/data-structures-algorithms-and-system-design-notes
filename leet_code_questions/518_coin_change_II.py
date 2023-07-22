b57200202780e77e7634f551b64bda9c2c71bbf3
Willie Shi <wshi9124@gmail.com>
2023-07-21 20:53:37 -0400

    518 dynamic programming

--- leet_code_questions/518_coin_change_II.py
+++ leet_code_questions/518_coin_change_II.py
@@ -47,4 +47,21 @@ def change(self, amount, coins):
     return dfs(0,0)
 
 
+    # DYNAMIC PROGRAMMING
+    # Time: O(n*m)
+    # Memory: O(n) where n = amount
 
+    dp = [0] * (amount + 1)
+    dp[0] = 1
+
+    for i in range(len(coins) - 1, -1, -1):
+        nextDP = [0] * (amount + 1)
+        nextDP[0] = 1
+
+        for a in range(1, amount + 1):
+            nextDP[a] = dp[a]
+            if a - coins[i] >= 0:
+                nextDP[a] += nextDP[a - coins[i]]
+        dp = nextDP
+
+    return dp[amount]
