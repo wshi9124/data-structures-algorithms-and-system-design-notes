ceea00e6643ea1badd75665d9977622fc9cab52f
Willie Shi <wshi9124@gmail.com>
2023-08-16 22:32:01 -0400

    73

--- leet_code_questions/73_set_matrix_zeroes.py
+++ leet_code_questions/73_set_matrix_zeroes.py
@@ -12,4 +12,24 @@ Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 """
 
 def setZeroes(self, matrix):
-        
+    # O(1) solution
+
+    rows, cols = len(matrix), len(matrix[0])
+    # check if first row is zero or not 
+    rowZero = False
+
+    #determine which rows/ cols need to be zero
+    for r in range(rows):
+        for c in range(cols):
+            if matrix[r][c] == 0:
+                matrix[0][c] = 0
+                if r > 0:
+                    matrix[0][r] = 0
+                else:
+                    rowZero = True
+
+    for r in range(rows):
+        for c in range(cols):
+            if matrix[0][c] == 0 or matrix[r][0] == 0:
+                
+
