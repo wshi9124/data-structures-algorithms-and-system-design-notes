2b7d1f83adbbd439ffcdc7070de3438a1d257d6b
Willie Shi <wshi9124@gmail.com>
2023-09-06 23:24:06 -0400

    leetcode

--- leet_code_questions/1851_minimum_interval_to_include_each_query.py
+++ leet_code_questions/1851_minimum_interval_to_include_each_query.py
@@ -25,12 +25,26 @@ Explanation: The queries are processed as follows:
 """
 
 def minInterval(self, intervals, queries):
+    # intervals.sort()
+
+    # result = []
+
+    # i = 0
+
+    # for q in sorted(queries):
     intervals.sort()
-
-    result = []
-
+    minHeap = []
+    res = {}
     i = 0
-
     for q in sorted(queries):
+        while i < len(intervals) and intervals[i][0] <= q:
+            l, r = intervals[i]
+            heapq.heappush(minHeap, (r - l + 1, r))
+            i += 1
+
+        while minHeap and minHeap[0][1] < q:
+            heapq.heappop(minHeap)
+        res[q] = minHeap[0][0] if minHeap else -1
+    return [res[q] for q in queries]
 
         
--- leet_code_questions/73_set_matrix_zeroes.py
+++ leet_code_questions/73_set_matrix_zeroes.py
@@ -12,22 +12,48 @@ Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 """
 
 def setZeroes(self, matrix):
-    # O(1) solution
+    # # O(1) solution
 
-    rows, cols = len(matrix), len(matrix[0])
-    # check if first row is zero or not 
+    # rows, cols = len(matrix), len(matrix[0])
+    # # check if first row is zero or not 
+    # rowZero = False
+
+    # #determine which rows/ cols need to be zero
+    # for r in range(rows):
+    #     for c in range(cols):
+    #         if matrix[r][c] == 0:
+    #             matrix[0][c] = 0
+    #             if r > 0:
+    #                 matrix[0][r] = 0
+    #             else:
+    #                 rowZero = True
+
+    # for r in range(rows):
+    #     for c in range(cols):
+    #         if matrix[0][c] == 0 or matrix[r][0] == 0:
+     # O(1)
+    ROWS, COLS = len(matrix), len(matrix[0])
     rowZero = False
 
-    #determine which rows/ cols need to be zero
-    for r in range(rows):
-        for c in range(cols):
+    # determine which rows/cols need to be zero
+    for r in range(ROWS):
+        for c in range(COLS):
             if matrix[r][c] == 0:
                 matrix[0][c] = 0
                 if r > 0:
-                    matrix[0][r] = 0
+                    matrix[r][0] = 0
                 else:
                     rowZero = True
 
-    for r in range(rows):
-        for c in range(cols):
+    for r in range(1, ROWS):
+        for c in range(1, COLS):
             if matrix[0][c] == 0 or matrix[r][0] == 0:
+                matrix[r][c] = 0
+
+    if matrix[0][0] == 0:
+        for r in range(ROWS):
+            matrix[r][0] = 0
+
+    if rowZero:
+        for c in range(COLS):
+            matrix[0][c] = 0
