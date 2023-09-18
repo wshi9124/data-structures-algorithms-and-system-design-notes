0e66d02b08859f292cf29b539222500fd1152cee
Willie Shi <wshi9124@gmail.com>
2023-09-17 22:50:05 -0400

    updated 271

--- leet_code_questions/271_encode_and_decode_strings.py
+++ leet_code_questions/271_encode_and_decode_strings.py
@@ -52,8 +52,8 @@ def encode(self, strs):
     """
     result = ""
     for s in strs:
-        result += str(len(s)) + "#" + "s"
-    return result
+        result += str(len(s)) + "#" + s
+    return result 
 
 def decode(self, s):
     """Decodes a single string to a list of strings.
@@ -61,15 +61,16 @@ def decode(self, s):
     :type s: str
     :rtype: List[str]
     """
-    result  = []
+    result = []
     i = 0
 
     while i < len(s):
         j = i
-        while s[i] != j:
-            j + 1
+        while s[j] != "#":
+            j += 1
         length = int(s[i:j])
-        result.append(s[j+1 : j + 1 + length])
-        i = j + 1 + length  
-    return result 
+        result.append(s[j+1: j+1+ length])
+        i = j + 1 + length
+    return result
+  
 
