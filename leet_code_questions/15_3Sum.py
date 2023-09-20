9aa015bbcf5ab823f665a5d119234b8c3565850b
Willie Shi <wshi9124@gmail.com>
2023-09-19 20:37:28 -0400

    updated 15

--- leet_code_questions/15_3Sum.py
+++ leet_code_questions/15_3Sum.py
@@ -27,26 +27,48 @@ def threeSum(self, nums):
     :type nums: List[int]
     :rtype: List[List[int]]
     """
+    # result = []
+    # nums.sort()
+
+    # for i, n in enumerate(nums):
+    #     if i > 0 and n == nums[i-1]:
+    #         continue
+        
+    #     l = i + 1
+    #     r = len(nums) - 1 
+
+    #     while l < r:
+    #         threeSum = n + nums[l] + nums[r]
+    #         if threeSum < 0:
+    #             l += 1
+    #         elif threeSum > 0:
+    #             r -= 1
+    #         else:
+    #             result.append([n, nums[l], nums[r]])
+    #             l += 1
+    #             while l < r and nums[l] == nums[l-1]:
+    #                 l += 1
+    
+    # return result
+
     result = []
     nums.sort()
 
     for i, n in enumerate(nums):
         if i > 0 and n == nums[i-1]:
             continue
-        
         l = i + 1
-        r = len(nums) - 1 
+        r = len(nums) - 1
 
         while l < r:
-            threeSum = n + nums[l] + nums[r]
-            if threeSum < 0:
-                l += 1
-            elif threeSum > 0:
+            sum = n + nums[l] + nums[r]
+            if sum > 0:
                 r -= 1
+            elif sum < 0:
+                l += 1
             else:
                 result.append([n, nums[l], nums[r]])
                 l += 1
-                while l < r and nums[l] == nums[l-1]:
+                while nums[l] == nums[l -1] and l < r:
                     l += 1
-    
     return result
