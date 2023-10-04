58179dbc2fc24b81e88501ee297602dfdf61dc95
Willie Shi <wshi9124@gmail.com>
2023-10-04 11:24:57 -0400

    updated 33

--- leet_code_questions/33_search_in_rotated_sorted_array.py
+++ leet_code_questions/33_search_in_rotated_sorted_array.py
@@ -26,27 +26,47 @@ def search(self, nums, target):
     :type target: int
     :rtype: int
     """
-    left = 0
-    right = len(nums) -1
+    # left = 0
+    # right = len(nums) -1
 
-    while left <= right:
-        mid = (right + left) //2
-        if target == nums[mid]:
-            return mid
+    # while left <= right:
+    #     mid = (right + left) //2
+    #     if target == nums[mid]:
+    #         return mid
 
-        #left sorted
-        if nums[left] <= nums[mid]:
-            if target > nums[mid] or target < nums[left]:
-                left = mid + 1
-            else:
-                right = mid - 1
+    #     #left sorted
+    #     if nums[left] <= nums[mid]:
+    #         if target > nums[mid] or target < nums[left]:
+    #             left = mid + 1
+    #         else:
+    #             right = mid - 1
 
-        #right sorted
-        else:
-            if target < nums[mid] or target > nums[right]:
-                right = mid -1
-            else:
-                left = mid + 1
+    #     #right sorted
+    #     else:
+    #         if target < nums[mid] or target > nums[right]:
+    #             right = mid -1
+    #         else:
+    #             left = mid + 1
     
-    return -1
+    # return -1
               
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        m = (l + r) // 2
+
+        if nums[m] == target:
+            return m
+        
+        if nums[l] <= nums[m]:
+            if target > nums[m] or target < nums[l]:
+                l = m + 1
+            else:
+                r = m - 1
+        else:
+            if target < nums[m] or target > nums[r]:
+                r = m - 1
+            else:
+                l = m + 1
+    
+    return -1 
