94950e6f47d7f32835ea6b3d6aac85630ce8d44e
Willie Shi <wshi9124@gmail.com>
2023-10-10 20:47:02 -0400

    updated 21

--- leet_code_questions/21_merge_two_sorted_lists.py
+++ leet_code_questions/21_merge_two_sorted_lists.py
@@ -32,22 +32,36 @@ def mergeTwoLists(list1, list2):
     Time complexity: l(n)
     Space complexity : O(1)O(1)O(1)
     """
-    dummy = ListNode()
-    tail = dummy 
+    # dummy = ListNode()
+    # tail = dummy 
+
+    # while list1 and list2:
+    #     if list1.val < list2.val:
+    #         tail.next = list1
+    #         list1 = list1.next
+    #     else:
+    #         tail.next = list2
+    #         list2 = list2.next
+    #     tail = tail.next    
+        
+    # if list1:
+    #     tail.next = list1
+    # elif list2:
+    #     tail.next = list2
+
+    # return dummy.next
+
+    dummy = node = ListNode()
 
     while list1 and list2:
         if list1.val < list2.val:
-            tail.next = list1
+            node.next = list1
             list1 = list1.next
         else:
-            tail.next = list2
+            node.next = list2
             list2 = list2.next
-        tail = tail.next    
-        
-    if list1:
-        tail.next = list1
-    elif list2:
-        tail.next = list2
+        node = node.next
+    node.next = list1 or list2
 
     return dummy.next
 
