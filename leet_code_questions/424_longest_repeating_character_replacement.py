244ad1a68a1c081d79d20421141f8ddb1054256d
Willie Shi <wshi9124@gmail.com>
2023-10-12 14:42:52 -0400

    updated 424

--- leet_code_questions/424_longest_repeating_character_replacement.py
+++ leet_code_questions/424_longest_repeating_character_replacement.py
@@ -27,16 +27,30 @@ def characterReplacement(self, s, k):
     #     result = max(result, r - l +1)
     # return result
 
+    # count = {}
+    # l = 0
+    # result = 0
+    # maxF = 0
+    # for r in range(len(s)):
+    #     count[s[r]] = 1 + count.get(s[r], 0)
+    #     maxF = max(maxF, count[s[r]])
+    #     # while window is invalid
+    #     while (r - l + 1) - maxF > k:
+    #         count[s[l]] -= 1
+    #         l += 1
+    #     result = max(result, r - l + 1)
+    # return result
+
     count = {}
-    l = 0
     result = 0
+    l = 0
     maxF = 0
+
     for r in range(len(s)):
         count[s[r]] = 1 + count.get(s[r], 0)
         maxF = max(maxF, count[s[r]])
-        # while window is invalid
-        while (r - l + 1) - maxF > k:
+        if (r - l + 1) - maxF > k:
             count[s[l]] -= 1
             l += 1
-        result = max(result, r - l + 1)
+        result = max(result, (r - l + 1))
     return result
