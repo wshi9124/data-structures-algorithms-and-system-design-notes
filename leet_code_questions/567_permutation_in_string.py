7e7ae85a062d11b2e9469663733c4b7d905eac23
Willie Shi <wshi9124@gmail.com>
2023-10-15 22:40:46 -0400

    updated 567

--- leet_code_questions/567_permutation_in_string.py
+++ leet_code_questions/567_permutation_in_string.py
@@ -14,8 +14,8 @@ Output: false
 
 def checkInclusion(self, s1, s2):
     if len(s1) > len(s2):
-            return False
-        
+        return False
+    
     s1Count = {"a":0, "b":0, "c":0, "d":0, "e":0, "f":0, "g":0, "h":0, "i":0, "j":0, "k":0, 
     "l":0, "m":0, "n":0, "o":0, "p":0, "q":0, "r":0, "s":0, "t":0, "u":0, "v":0, "w":0, "x":0, "y":0, "z":0}
 
@@ -28,11 +28,11 @@ def checkInclusion(self, s1, s2):
     
     if s1Count == s2Count:
         return True
-    
+        
     l = 0
-    r = len(s1) -1
+    r = len(s1) - 1
 
-    while r < len(s2) -1:
+    while r < len(s2) - 1:
         s2Count[s2[l]] -= 1
         l += 1
         r += 1
@@ -40,3 +40,5 @@ def checkInclusion(self, s1, s2):
         if s1Count == s2Count:
             return True
     return False
+
+
