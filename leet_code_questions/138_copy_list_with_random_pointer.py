2cdc64bc32030f247e9c86f8af18fb60d6702b9f
Willie Shi <104730743+wshi9124@users.noreply.github.com>
2023-10-21 22:37:34 -0400

    138

--- leet_code_questions/138_copy_list_with_random_pointer.py
+++ leet_code_questions/138_copy_list_with_random_pointer.py
@@ -26,19 +26,6 @@ def copyRandomList(self, head):
     :type head: Node
     :rtype: Node
     """
-    oldToCopy = {None: None}
+    oldToCopy = { None: None }
 
-    curr = head
-    while curr:
-        copy = Node(curr.val)
-        oldToCopy[curr] = copy
-        curr = curr.next
     
-    curr = head
-    while curr:
-        copy = oldToCopy[curr]
-        copy.next = oldToCopy[curr.next]
-        copy.random = oldToCopy[curr.random]
-        curr = curr.next
-
-    return oldToCopy[head]
