f7f36b0a0d57c3d19b658f5963545688cb8e3d2d
Willie Shi <104730743+wshi9124@users.noreply.github.com>
2023-10-24 13:24:06 -0400

    4

--- leet_code_questions/4_median_of_two_sorted_arrays.py
+++ leet_code_questions/4_median_of_two_sorted_arrays.py
@@ -12,35 +12,41 @@ Input: nums1 = [1,2], nums2 = [3,4]
 Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 """
-def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+def findMedianSortedArrays(self, nums1, nums2):
     a, b = nums1, nums2
-    total = len(a) + len(b) 
-    half = total //2
+    total = len(a) + len(b)
+    half = total // 2
 
-    if len(a) > len(b):
+    #find lower length
+    if len(b) < len(a):
         a,b = b,a
     
-    l, r = 0, len(a) -1
+    l, r = 0, len(a) - 1
+
     while True:
-        i = (l +r)//2 #a
+        i = (l + r) //2 #a
         j = half - i - 2 #b
-        #float infinity for edge case if none of the numbers are in the first array
+
         aLeft = a[i] if i >= 0 else float("-infinity")
-        aRight = a[i+1] if i+1 < len(a) else float("infinity")
+        aRight = a[i + 1] if (i+1) < len(a) else float("infinity")
         bLeft = b[j] if j >= 0 else float("-infinity")
-        bRight = b[j+1] if j+1 < len(b) else float("infinity")
+        bRight = b[j + 1] if (j+1) < len(b) else float("infinity")
 
         if aLeft <= bRight and bLeft <= aRight:
             #odd
-            if total % 2 != 0:
+            if total % 2:
                 return min(aRight, bRight)
             #even
             return (max(aLeft, bLeft) + min(aRight, bRight)) /2
         elif aLeft > bRight:
-            r = i -1
+            r = i - 1
         else:
             l = i + 1
+        
 
+        
 
 
     
+
+
