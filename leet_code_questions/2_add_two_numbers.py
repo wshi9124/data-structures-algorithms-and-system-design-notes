93f4287522f4c28520cf782ddc27c4bc2cf7232d
Willie Shi <104730743+wshi9124@users.noreply.github.com>
2023-10-26 16:26:06 -0400

    updated 2

--- leet_code_questions/2_add_two_numbers.py
+++ leet_code_questions/2_add_two_numbers.py
@@ -22,20 +22,23 @@ def addTwoNumbers(self, l1, l2):
     :rtype: ListNode
     """
     dummy = ListNode()
-    cur = dummy
+    curr = dummy
 
     carry = 0
+
     while l1 or l2 or carry:
         v1 = l1.val if l1 else 0
-        v2 = l2.val if l2 else 0 
-        val = v1 + v2 + carry
-        carry = val // 10
-        val = val % 10
-        cur.next = ListNode(val)
-        cur = cur.next
+        v2 = l2.val if l2 else 0
+
+        #new digit
+        total = v1 + v2 + carry
+        carry = total // 10
+        val = total % 10 
+        curr.next = ListNode(val)
+
+        #move pointers
         l1 = l1.next if l1 else None
         l2 = l2.next if l2 else None
-    
-    return dummy.next
+        curr = curr.next
 
-        
+    return dummy.next
