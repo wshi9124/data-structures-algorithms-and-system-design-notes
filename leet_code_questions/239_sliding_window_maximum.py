3a1c46b11dc2f020b782e1f0fbcfe8c8754648ab
Willie Shi <104730743+wshi9124@users.noreply.github.com>
2023-11-01 17:36:16 -0400

    updated 239

--- leet_code_questions/239_sliding_window_maximum.py
+++ leet_code_questions/239_sliding_window_maximum.py
@@ -23,25 +23,27 @@ Output: [1]
 def maxSlidingWindow(self, nums, k):
     # Monotonically Decreasing Que
     # o(n) time
-    output = []
-    q = collections.deque() #holds only index
 
+    output = [] #index
+    
+    q = collections.deque()
+            
     l = r = 0
 
     while r < len(nums):
-        # pop smaller values from q
+        #pop smaller values from q
         while q and nums[q[-1]] < nums[r]:
-            q.pop()
+            q.pop() 
         q.append(r)
 
-        # remove left val from window
+        #remove left from window:
         if l > q[0]:
             q.popleft()
-
+        
         if (r + 1) >= k:
             output.append(nums[q[0]])
             l += 1
         r += 1
-
+    
     return output
-            
+
