023e714591e9958447748029808561d8d511fd7b
Willie Shi <104730743+wshi9124@users.noreply.github.com>
2023-11-10 00:24:19 -0500

    updated 100

--- leet_code_questions/100_same_Tree.py
+++ leet_code_questions/100_same_Tree.py
@@ -31,4 +31,7 @@ def isSameTree(self, p, q):
     if not p and q:
         return True
     
-    if p and q
+    if p and q and p.val == q.val:
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    
+    return False
