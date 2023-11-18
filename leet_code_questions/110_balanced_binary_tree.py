2adaca3731ae9a3402e1dd7d18aaa6ef6e3f997d
Willie Shi <104730743+wshi9124@users.noreply.github.com>
2023-11-17 22:02:46 -0500

    updated 110

--- leet_code_questions/110_balanced_binary_tree.py
+++ leet_code_questions/110_balanced_binary_tree.py
@@ -18,14 +18,22 @@ Output: true
 """
 
 def isBalanced(self, root):
-    def dfs(root):
-        if not root: return [True, 0]
+    # def dfs(root):
+    #     if not root: return [True, 0]
     
-        left, right = dfs(root.left), dfs(root.right)
-        #need to check if left and right is balanced (true)
-        balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+    #     left, right = dfs(root.left), dfs(root.right)
+    #     #need to check if left and right is balanced (true)
+    #     balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
 
-        return [balanced, 1 + max(left[1], right[1])]
+    #     return [balanced, 1 + max(left[1], right[1])]
     
-    return dfs(root)[0]
+    # return dfs(root)[0]
+
+    def dfs(root):
+        if not root:
+            return [True, 0]
+        
+        left, right = dfs(root.left), dfs(root.right)
+        balanced = abs(left[1] - right[1])
+
 
