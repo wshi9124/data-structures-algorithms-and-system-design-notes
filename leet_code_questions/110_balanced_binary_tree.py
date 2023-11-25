afbec0d4d22f8e2d2ac4a01092028c33fd85aa0f
Willie Shi <104730743+wshi9124@users.noreply.github.com>
2023-11-24 21:49:38 -0500

    updated 110

--- leet_code_questions/110_balanced_binary_tree.py
+++ leet_code_questions/110_balanced_binary_tree.py
@@ -34,6 +34,9 @@ def isBalanced(self, root):
             return [True, 0]
         
         left, right = dfs(root.left), dfs(root.right)
-        balanced = abs(left[1] - right[1])
+        balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+        return [balanced, 1 + max(left[1], right[1])]
+    
+    return dfs(root)[0]
 
 
