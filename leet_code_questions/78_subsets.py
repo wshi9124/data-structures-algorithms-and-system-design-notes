b488c93ab16f829d9e76f9db93b41c69ecdabc1d
Willie Shi <104730743+wshi9124@users.noreply.github.com>
2024-01-04 16:06:54 -0500

    78

--- leet_code_questions/78_subsets.py
+++ leet_code_questions/78_subsets.py
@@ -19,32 +19,32 @@ def subsets(self, nums):
     * so no [2,1]
     """
 
-    result = []
-
-    def dfs(i, subset):
-        if i >= len(nums):
-            result.append(subset)
-            return
-        dfs(i+1, subset + [nums[i]])
-        dfs(i+1, subset)
-
-    dfs(0, [])
-    return result
-
     # result = []
-    # subset = []
 
-    # def dfs(i):
+    # def dfs(i, subset):
     #     if i >= len(nums):
-    #         #need .copy because subset is going to be modified
-    #         result.append(subset.copy())
+    #         result.append(subset)
     #         return
-    #     #decision to include nums[i]
-    #     subset.append(nums[i])
-    #     dfs(i+1)
-    #     # decision not to include nums[i]
-    #     subset.pop()
-    #     dfs(i+1)
-    # dfs(0)
+    #     dfs(i+1, subset + [nums[i]])
+    #     dfs(i+1, subset)
+
+    # dfs(0, [])
     # return result
+
+    result = []
+    subset = []
+
+    def dfs(i):
+        if i >= len(nums):
+            #need .copy because subset is going to be modified
+            result.append(subset.copy())
+            return
+        #decision to include nums[i]
+        subset.append(nums[i])
+        dfs(i+1)
+        # decision not to include nums[i]
+        subset.pop()
+        dfs(i+1)
+    dfs(0)
+    return result
         
